58461e7c9f1c62f8f71ea525ae73a77eb88c3cb0
Buhle <mtshalizanobuhle7@gmail.com>
2025-01-02 09:59:56 +0200

    updated the javascript file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //defining variables
 let tabsArray = []; //this is where we will store all variables
 const inputElement = document.getElementById('input-bar');
+let ulContainer = document.getElementById('list-container')
 const saveLinkBtn = document.getElementById('save-url');
 const saveTabBtn = document.getElementById('save-tab');
 const clearAllBtn = document.getElementById('clear-all');
@@ -8,5 +9,25 @@ const clearAllBtn = document.getElementById('clear-all');
 
 //first the function to push input value to the array
 saveLinkBtn.addEventListener('click', () => {
-    console.log('save button clicked')
+    tabsArray.push(inputElement.value);
+    inputElement.value = '';
+    render(tabsArray);
 })
+
+
+//function for rendering out the input elements
+function render(items){
+    let listItems = '';
+    for (let i = 0; i < items.length; i++){
+        listItems += `
+                       <div class="lists">
+                        <li>${items[i]}</li>
+                        <span class="material-icons">
+                        delete_outline
+                        </span>
+                        </div> 
+                    `
+    }
+
+    ulContainer.innerHTML = listItems;
+}
