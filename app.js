a95e0ce2bb7a89bea74291245bbdbd35654cb63e
Buhle <mtshalizanobuhle7@gmail.com>
2025-01-02 09:50:45 +0200

    updated the javscript file with variables and added the first function

---
+++ app.js
@@ -0,0 +1,12 @@
+//defining variables
+let tabsArray = []; //this is where we will store all variables
+const inputElement = document.getElementById('input-bar');
+const saveLinkBtn = document.getElementById('save-url');
+const saveTabBtn = document.getElementById('save-tab');
+const clearAllBtn = document.getElementById('clear-all');
+
+
+//first the function to push input value to the array
+saveLinkBtn.addEventListener('click', () => {
+    console.log('save button clicked')
+})
